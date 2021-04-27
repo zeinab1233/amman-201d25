5a2a11869004492e7f2c22aab47efe132d121e57
zeinab1233 <zeinab.albadareen@gmail.com>
2021-04-28 02:50:57 +0300

    drink

---
+++ app.js
@@ -0,0 +1,38 @@
+/*
+this is a comment in the js file 
+var variable
+let 
+const
+*/
+
+// let baraah = 'baraah';
+// // it will give me the authority to declare the variable name only one time
+
+// let baraah = 'Ola';
+// console.log(baraah);
+
+// I cannot use it before initialize it 
+// let car;
+// car = 'BMW';
+// console.log(car);
+
+
+// const I can declare it and use it only one time
+// const url = 'https://github.com/';
+// console.log(url);
+
+// const url = 'baraa\'h';
+
+
+let favdrink = prompt('what\'s your favdrink');
+console.log(favdrink);
+
+let typeofvitamin = prompt('what type of vitamins does your favourite drink contain?');
+console.log(typeofvitamin);
+
+let benefits = prompt('what is the benifits of your fav drink?');
+console.log(benefits);
+let alternatives = prompt('what is the other alternative of your fav drink?');
+console.log(alternatives);
+
+alert('your favourite drink is ' + favdrink + 'great it contains vitamin ' + typeofvitamin + ' and its good for your ' + benefits + ' and you also like ' + alternatives + ' as an alternative ' );
